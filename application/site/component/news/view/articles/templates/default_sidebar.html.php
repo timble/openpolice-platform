9a3ed22052afcaf800dbe2a50ed75b67cb170366
Tom Janssens <tom@timble.net>
2013-05-26 00:44:35 +0200

    Improved content #1

--- application/site/component/news/view/articles/templates/default_sidebar.html.php
+++ application/site/component/news/view/articles/templates/default_sidebar.html.php
@@ -1,9 +1,9 @@
 <? $site = @object('application')->getCfg('site') ?>
 
-<h3><i class="icon-question-sign"></i> Vragen</h3>
-<p>Meer weten over cybercrime of drugs? Of wat de politie doet tegen woninginbraak?</p>
+<h3><i class="icon-question-sign"></i> Veelgestelde vragen</h3>
+<p>Hebt u een vraag? Zoek een antwoord tussen tal van andere veelgestelde vragen.</p>
 <p><a href="<?= $site ?>/vragen">Beantwoord uw vraag &rarr;</a></p>
 <hr />
-<h3><i class="icon-road"></i>Verkeer</h3>
-<p>Bent u opzoek naar informatie over controle acties of verkeersmaatregelen?</p>
-<p><a href="<?= $site ?>/verkeer">Bekijk verkeersinformatie &rarr;</a></p>
+<h3><i class="icon-road"></i>Verkeersinformatie</h3>
+<p>Opzoek naar informatie over controles, verkeersmaatregelen of wegenwerken?</p>
+<p><a href="<?= $site ?>/verkeer">Bekijk de verkeersinformatie &rarr;</a></p>
--- application/site/component/police/view/page/templates/homepage.html.php
+++ application/site/component/police/view/page/templates/homepage.html.php
@@ -53,21 +53,21 @@
     <div class="span3">
         <h3><i class="icon-file-alt"></i> Aangifte doen</h3>
         <p>Aangifte doen is belangrijk, alleen dan kan de politie een onderzoek starten.</p>
-        <p><a href="<?= $site ?>/aangifte">Doe aangifte &rarr;</a></p>
+        <p><a href="<?= $site ?>/aangifte">Doe een aangifte &rarr;</a></p>
     </div>
     <div class="span3">
-        <h3><i class="icon-question-sign"></i> Vragen</h3>
-        <p>Meer weten over cybercrime of drugs? Of wat de politie doet tegen woninginbraak?</p>
+        <h3><i class="icon-question-sign"></i> Veelgestelde vragen</h3>
+        <p>Hebt u een vraag? Zoek een antwoord tussen tal van andere veelgestelde vragen.</p>
         <p><a href="<?= $site ?>/vragen">Beantwoord uw vraag &rarr;</a></p>
     </div>
     <div class="span3">
-        <h3><i class="icon-road"></i>Verkeer</h3>
-        <p>Bent u opzoek naar informatie over controle acties of verkeersmaatregelen?</p>
-        <p><a href="<?= $site ?>/verkeer">Bekijk verkeersinformatie &rarr;</a></p>
+        <h3><i class="icon-road"></i>Verkeersinformatie</h3>
+        <p>Opzoek naar informatie over controles, verkeersmaatregelen of wegenwerken?</p>
+        <p><a href="<?= $site ?>/verkeer">Bekijk de verkeersinformatie &rarr;</a></p>
     </div>
     <div class="span3">
-        <h3><i class="icon-group"></i> Verloren voorwerpen</h3>
+        <h3><i class="icon-key"></i> Verloren voorwerpen</h3>
         <p>Iets verloren? Misschien hebben wij je verloren voorwerp gevonden.</p>
-        <p><a href="#">Verloren voorwerpen &rarr;</a></p>
+        <p><a href="#">Zoek uw verloren voorwerpen &rarr;</a></p>
     </div>
 </div>
