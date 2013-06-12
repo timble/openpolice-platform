5c38f9861f445c4061614456360ddc9748ef8b3d
Tom Janssens <tom@timble.net>
2013-06-12 22:29:22 +0200

    Content update #1

--- application/site/component/police/view/page/templates/homepage.html.php
+++ application/site/component/police/view/page/templates/homepage.html.php
@@ -57,17 +57,17 @@
     </div>
     <div class="span3">
         <h3><i class="icon-question-sign"></i> Veelgestelde vragen</h3>
-        <p>Hebt u een vraag? Zoek een antwoord tussen tal van andere veelgestelde vragen.</p>
-        <p><a href="<?= $site ?>/vragen">Beantwoord uw vraag &rarr;</a></p>
+        <p>Heb je een vraag? Zoek een antwoord tussen veelgestelde vragen.</p>
+        <p><a href="<?= $site ?>/vragen">Beantwoord je vraag &rarr;</a></p>
     </div>
     <div class="span3">
         <h3><i class="icon-road"></i>Verkeersinformatie</h3>
-        <p>Opzoek naar informatie over controles, verkeersmaatregelen of wegenwerken?</p>
+        <p>Op zoek naar informatie over controles, verkeersmaatregelen of wegenwerken?</p>
         <p><a href="<?= $site ?>/verkeer">Bekijk de verkeersinformatie &rarr;</a></p>
     </div>
     <div class="span3">
         <h3><i class="icon-key"></i> Verloren voorwerpen</h3>
         <p>Iets verloren? Misschien hebben wij je verloren voorwerp gevonden.</p>
-        <p><a href="#">Zoek uw verloren voorwerpen &rarr;</a></p>
+        <p><a href="#">Zoek je verloren voorwerpen &rarr;</a></p>
     </div>
 </div>
