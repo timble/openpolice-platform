77529b51c0d9ea51dadca85f02bf1aed616b7f75
Tom Janssens <tom@timble.net>
2013-06-23 16:00:23 +0200

    Replaced 112 slogan #1

--- application/site/component/police/view/page/templates/homepage.html.php
+++ application/site/component/police/view/page/templates/homepage.html.php
@@ -35,7 +35,7 @@
     </div>
     <div class="span4">
         <div class="box">
-            <p><strong><i class="icon-phone"></i> 101</strong> voor dringende hulp</p>
+            <p><strong><i class="icon-phone"></i> 101</strong> voor dringende politiehulp</p>
             <p><strong><i class="icon-phone"></i> <?= $zone->telephone ?></strong> geen spoed</p>
             <hr />
             <h3><i class="icon-comments-alt"></i> Contacteer ons</h3>
