26360cf7488b6cb0d4b567a76494568b50285dff
Tom Janssens <tom@timble.net>
2013-07-01 19:32:26 +0200

    Cleanup #1

--- application/site/public/theme/mobile/js/flaunt.js
+++ application/site/public/theme/mobile/js/flaunt.js
@@ -1,38 +1,18 @@
 /*
-	Inspired by Flaunt.js v1.0.0
-	by Todd Motto: http://www.toddmotto.com
-	Latest version: https://github.com/toddmotto/flaunt-js
-	
-	Copyright 2013 Todd Motto
-	Licensed under the MIT license
-	http://www.opensource.org/licenses/mit-license.php
-
-	Flaunt JS, stylish responsive navigations with nested click to reveal.
+	Inspired by Flaunt.js - https://github.com/toddmotto/flaunt-js
 */
 ;(function($) {
 	// DOM ready
 	$(function() {
-		// Append the mobile icon nav
-		$('.navbar').prepend($('<div class="nav-mobile"></div>'));
+		// Append the handle
+		$('.navbar').prepend($('<div class="navbar__handle"></div>'));
 
+        // Hide the menu so it does not depend on JS support
         $('.nav').addClass("is-hidden");
-
-		// Add a <span> to every .nav-item that has a <ul> inside
-		$('.nav li').has('ul').prepend('<span class="nav-click"><i class="nav-arrow"></i></span>');
 		
-		// Click to reveal the nav
-		$('.nav-mobile').click(function(){
+		// Click to show the navigation
+		$('.navbar__handle').click(function(){
             $('.navbar .navbar-inner > .nav').toggleClass('is-hidden');
-		});
-	
-		// Dynamic binding to on 'click'
-		$('.nav').on('click', '.nav-click', function(){
-		
-			// Toggle the nested nav
-			$(this).siblings('ul').toggle();
-			
-			// Toggle the arrow using CSS3 transforms
-			$(this).children('.nav-arrow').toggleClass('nav-rotate');
 		});
 	});
 })(jQuery);
