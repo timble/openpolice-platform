b1dcc96ab73af8aa5aae7d8b9eb0ca157fcbf727
Tom Janssens <tom@timble.net>
2013-07-08 00:08:01 +0200

    Minor improvements #1

--- application/site/component/police/view/page/templates/homepage.html.php
+++ application/site/component/police/view/page/templates/homepage.html.php
@@ -14,21 +14,21 @@
     <div class="span8 hidden-phone">
         <? foreach (@object('com:news.model.articles')->sticky(true)->getRowset() as $article) : ?>
             <div class="page-header">
-                <h1><a href="<?= $site.'/nieuws/'.$article->id.'-'.$article->slug ?>"><?= $article->title ?></a></h1>
+                <h1><a href="<?= '/'.$site.'/nieuws/'.$article->id.'-'.$article->slug ?>"><?= $article->title ?></a></h1>
                 <span class="timestamp">
                     <?= @helper('date.format', array('date'=> $article->ordering_date, 'format' => JText::_('DATE_FORMAT_LC5'))) ?>
                 </span>
             </div>
 
             <div class="clearfix">
-                <? if($article->thumbnail): ?>
-                    <img class="thumbnail" src="<?= $article->thumbnail ?>" />
-                <? endif; ?>
+                <a href="<?= '/'.$site.'/nieuws/'.$article->id.'-'.$article->slug ?>">
+                    <?= @helper('com:attachments.image.thumbnail', array('row' => $article)) ?>
+                </a>
 
                 <?= $article->introtext ?>
 
                 <? if ($article->fulltext) : ?>
-                    <a href="<?= $site.'/nieuws/'.$article->id.'-'.$article->slug ?>"><?= @text('Read more') ?></a>
+                    <a href="<?= '/'.$site.'/nieuws/'.$article->id.'-'.$article->slug ?>"><?= @text('Read more') ?></a>
                 <? endif; ?>
             </div>
         <? endforeach; ?>
@@ -38,8 +38,7 @@
             <p><strong><i class="icon-phone"></i> 101</strong> voor dringende politiehulp</p>
             <p><strong><i class="icon-phone"></i> <?= $zone->telephone ?></strong> geen spoed</p>
             <hr />
-            <h3><i class="icon-comments-alt"></i> Contacteer ons</h3>
-            <ul class="nav">
+            <ul class="nav nav-pills nav-stacked">
                 <li><a href="/<?= $site ?>/contact/commissariaten">Commissariaten</a></li>
                 <li><a href="/<?= $site ?>/contact/je-wijkinspecteur">Je wijkinspecteur</a></li>
                 <li><a href="/<?= $site ?>/contact/diensten">Diensten</a></li>
