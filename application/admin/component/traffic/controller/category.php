7eb4b09e19a079c8efef51b0bce7d880ed8ad6ff
Tom Janssens <tom@timble.net>
2013-08-10 17:34:01 +0200

    Making categories view accessible #1

---
+++ application/admin/component/traffic/controller/category.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Belgian Police Web Platform - Traffic Component
+ *
+ * @copyright	Copyright (C) 2012 - 2013 Timble CVBA. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		http://www.police.be
+ */
+
+use Nooku\Library;
+
+class TrafficControllerCategory extends CategoriesControllerCategory
+{
+
+}
