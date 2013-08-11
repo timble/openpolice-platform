725ccb52ea010037100015874c0c873db723a6a3
Tom Janssens <tom@timble.net>
2013-08-11 14:31:53 +0200

    Docblocks

--- application/admin/component/traffic/bootstrapper.php
+++ application/admin/component/traffic/bootstrapper.php
@@ -1,21 +1,14 @@
 <?php
 /**
- * @package     Nooku_Server
- * @subpackage  Weblinks
- * @copyright   Copyright (C) 2007 - 2012 Johan Janssens. All rights reserved.
- * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
- * @link        http://www.nooku.org
+ * Belgian Police Web Platform - Traffic Component
+ *
+ * @copyright	Copyright (C) 2012 - 2013 Timble CVBA. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		http://www.police.be
  */
 
 use Nooku\Library;
 
-/**
- * Weblinks Bootstrapper
- *
- * @author      Johan Janssens <johan@nooku.org>
- * @package     Nooku_Server
- * @subpackage  Weblinks
- */
 class TrafficBootstrapper extends Library\BootstrapperAbstract
 {
     public function bootstrap()
