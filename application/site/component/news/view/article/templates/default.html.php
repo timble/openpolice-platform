c3178723b1db1be1c593aed8b03186d9564e6c2c
Tom Janssens <tom@timble.net>
2013-08-15 16:18:23 +0200

    Markup #1

--- application/site/component/news/view/article/templates/default.html.php
+++ application/site/component/news/view/article/templates/default.html.php
@@ -11,17 +11,19 @@
 <script type="text/javascript" src="http://w.sharethis.com/button/buttons.js"></script>
 <script type="text/javascript">stLight.options({publisher:'91c73e48-a5e0-43ea-988f-57d099f878c7'});</script>
 
-<div class="page-header clearfix">
-    <h1><?= $article->title ?></h1>
-    <span class="timestamp">
-        <?= @helper('date.format', array('date'=> $article->ordering_date, 'format' => JText::_('DATE_FORMAT_LC5'))) ?>
-    </span>
-    <span style="float:right" class='st_sharethis' displayText='ShareThis'></span>
-</div>
+<article>
+    <div class="page-header clearfix">
+        <h1><?= $article->title ?></h1>
+        <span class="timestamp">
+            <?= @helper('date.format', array('date'=> $article->ordering_date, 'format' => JText::_('DATE_FORMAT_LC5'))) ?>
+        </span>
+        <span style="float:right" class='st_sharethis' displayText='ShareThis'></span>
+    </div>
 
-<?= @helper('com:attachments.image.thumbnail', array('row' => $article)) ?>
+    <?= @helper('com:attachments.image.thumbnail', array('row' => $article)) ?>
 
-<div class="article__introtext"><?= $article->introtext ?></div>
-<?= $article->fulltext ?>
+    <div class="article__introtext"><?= $article->introtext ?></div>
+    <?= $article->fulltext ?>
 
-<?= @template('com:attachments.view.attachments.default.html', array('attachments' => $attachments, 'exclude' => array($article->attachments_attachment_id))) ?>
+    <?= @template('com:attachments.view.attachments.default.html', array('attachments' => $attachments, 'exclude' => array($article->attachments_attachment_id))) ?>
+</article>
