290bb5e825464b1d22b0567b638bf7782c314d0a
Steven Rombauts <steven@timble.net>
2013-08-21 13:17:32 +0100

    Capistrano: add staging branch, re-enable New Relic hook

--- scripts/capistrano/config/deploy/production.rb
+++ scripts/capistrano/config/deploy/production.rb
@@ -5,4 +5,4 @@ server "police-production", :app, :web, :db, :primary => true
 set :rails_env, "production"
 set :branch, "master"
 
-# after "deploy:update", "newrelic:notice_deployment"
+after "deploy:update", "newrelic:notice_deployment"
