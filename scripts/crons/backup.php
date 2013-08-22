12c0168a884e6ad3b1dc54766a0c7bca0dd1f56a
Steven Rombauts <steven@timble.net>
2013-08-22 12:16:05 +0100

    Include backup and sync scripts

---
+++ scripts/crons/backup.php
@@ -0,0 +1,244 @@
+<?php
+define('DS', DIRECTORY_SEPARATOR);
+
+require_once '../../config/config.php';
+
+$config = new JConfig();
+$backup = new Backup('/var/backups/');
+
+// Step 1 - create the daily dumps
+$backup->dumpDatabases($config);
+
+// Step 2 - execute the weekly rotation on the database dumps every sunday
+if(date('N') == 7)
+{
+    $filename = 'databases'.DS.'weekly'.DS.'platform-'.date('Y-\w\e\e\kW').'.'.date('Ymd').'.tgz';
+    $backup->rotate('databases'.DS.'daily', 'tar.gz', $filename);
+}
+
+// Step 3 - execute the monthly rotation on the database dumps, the last day of each month
+if(date('j') == date('t'))
+{
+    $filename = 'databases'.DS.'monthly'.DS.'platform-'.date('Ymd').'.tgz';
+    $backup->rotate('databases'.DS.'weekly', 'tgz', $filename);
+}
+
+// Step 4 - clean-up backups
+$backup->cleanup('databases'.DS.'daily', 7); // remove dumps older than 7 days
+$backup->cleanup('databases'.DS.'weekly', 62); // remove weekly rotations older than 2 months
+$backup->cleanup('databases'.DS.'monthly', 365); // remove monthly rotations older than 1 year
+
+/**
+ * Backup class
+ *
+ */
+class Backup
+{
+    /**
+     * @var string	base path of the stored and rotated backups
+     */
+    protected $_backup_directory;
+
+    public function __construct()
+    {
+        if(func_num_args())	{
+            $this->_backup_directory = func_get_arg(0);
+        }
+    }
+
+    /**
+     * Creates a MySQL dump file of all databases
+     *
+     * @param string $filename	name of dump output file
+     * @return string the full path to the backup file
+     */
+    public function dumpDatabases($config)
+    {
+        $username = $config->user;
+        $password = $config->password;
+
+        $filename = 'databases'.DS.'daily'.DS.'platform.'.date('Ymd').'.sql.tar';
+        $filename = $this->_getPath($filename);
+
+        // Get a list of all the databases
+        $cmd = 'mysql -u'.escapeshellarg($username).' -p'.escapeshellarg($password);
+        $cmd .= ' --raw --skip-column-names -e "SHOW DATABASES;"';
+
+        exec($cmd, $databases);
+
+        foreach($databases as $key => $database)
+        {
+            if(substr($database, 0, strlen('v2_')) != 'v2_') {
+                unset($databases[$key]);
+            }
+        }
+
+        // Create an empty tarball
+        exec('tar -cvf '.$filename.' --files-from /dev/null');
+
+        // Now add each database dump into the tarball
+        $tmp = DS.'tmp'.DS.uniqid().DS;
+        mkdir($tmp);
+
+        foreach($databases as $database)
+        {
+            $destination = $tmp.$database.'.sql';
+
+            $cmd = 'mysqldump --complete-insert --add-drop-table --extended-insert --quote-names';
+            $cmd .= ' -u'.escapeshellarg($username).' -p'.escapeshellarg($password).' '.escapeshellarg($database);
+            $cmd .= ' > ' . $destination;
+
+            exec($cmd);
+
+            $cmd = 'tar -rvf '.$filename.' -C '.$tmp.' ' . $database.'.sql';
+            exec($cmd);
+
+            unlink($destination);
+        }
+
+        rmdir($tmp);
+
+        // Make sure to compress the resulting tarball
+        $cmd = 'gzip -f ' . $filename;
+        exec($cmd);
+
+        return $filename;
+    }
+
+    /**
+     * Creates a tarball compressed with gzip containing everything in source
+     *
+     * @return string file path of the backup
+     */
+    public function archiveFiles($source)
+    {
+        $filename = 'files'.DS.'platform.'.date('Ymd').'.tgz';
+        $filename = $this->_getPath($filename);
+
+        if(!file_exists($source)) {
+            throw Exception('Cannot find ' . $source . ' for archiving');
+        }
+
+        $cmd = 'tar -cvzf '.$filename.' ' .$source;
+        exec($cmd);
+    }
+
+    /**
+     * Rotates the specified files with extension $src_extension in
+     * the $source directory, and creates a tar file at given $filename path
+     *
+     * @param string $source
+     * @param string $src_extension
+     * @param string $filename
+     */
+    public function rotate($source, $src_extension, $filename)
+    {
+        $source = $this->_getPath($source);
+        $filename = $this->_getPath($filename);
+
+        $cmd = 'cd '.$source.' && tar -cvzf '.$filename.' `ls *.'.$src_extension.'`';
+
+        return exec($cmd);
+    }
+
+    /**
+     * Deletes all files older than $age (in days) in $directory
+     * Instead of using the find command, we validate through PHP
+     * which file needs to be kept. The reason for this is because of
+     * CloudFuse's inability to display each files' modified time correctly!
+     *
+     * It's important to note that we rely on a timestamp, with format Ymd or YmdHis,
+     * to determine the file's age. This timestamp must occur just before the file's extension,
+     * for example: backup.weekly.20120801.tgz
+     *
+     * @param string	$directory
+     * @param string	$age (in days)
+     */
+    public function cleanup($directory, $age)
+    {
+        $directory = $this->_getPath($directory);
+
+        if($handle = opendir($directory))
+        {
+            while(($file = readdir($handle)) !== false)
+            {
+                if($file == '.' || $file == '..') {
+                    continue;
+                }
+
+                $time = $this->_getTimestampFromFile($file);
+
+                if($time !== false && $time < strtotime('-'.$age.' days')) {
+                    unlink($directory.DS.$file);
+                }
+            }
+
+            closedir($handle);
+
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Adds base backup directory if given path is an absolute path
+     *
+     * @param string $filename
+     */
+    protected function _getPath($filename)
+    {
+        if(substr($filename, 0, 1) != '/') {
+            $filename = $this->_backup_directory.DS.$filename;
+        }
+
+        $this->_createDirectories($filename);
+
+        return $filename;
+    }
+
+    /**
+     * Makes sure all parent paths exists
+     *
+     * @param string $filename
+     */
+    protected function _createDirectories($path)
+    {
+        $directory = dirname($path);
+        $exists = file_exists($directory);
+
+        if(!$exists)
+        {
+            $success = mkdir($directory, 0755, true);
+
+            if(!$success) {
+                throw new Exception('Could not create ' . $directory);
+            }
+
+            return $success;
+        }
+
+        return $exists;
+    }
+
+    /**
+     * Extracts the timestamp from a given filename.
+     *
+     * @param string $filename
+     */
+    protected function _getTimestampFromFile($filename)
+    {
+        $parts = explode('.', $filename);
+
+        $len = count($parts);
+        $date = $parts[$len-2];
+
+        $stamp = strtotime($date);
+
+        if(!$stamp) {
+            return false;
+        }
+
+        return $stamp;
+    }
+}
---
+++ scripts/crons/sync.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * This script will fetch the latest database dumps
+ * and sync the source files with production to synchronize
+ * the staging server with the current live version.
+ *
+ * Make sure staging's root can SSH to production as the deploy user
+ * using public key authentication.
+ */
+
+require_once '../../config/config.php';
+
+$config = new JConfig();
+
+$tmp = '/tmp/'.uniqid();
+
+// Start with the mysql dumps: get the most recent one from
+// the production backups and import every db
+echo "-- Syncing MySQL databases" . PHP_EOL;
+$dumps_dir = $tmp.'/mysql/';
+mkdir($dumps_dir, 0755, true) or exit('Could not create ' . $dumps_dir);
+chdir($dumps_dir) or exit('Could not change working directory to' . $dumps_dir);
+
+// Get the latest file name
+$filename = exec("ssh -p 9999 deploy@172.18.150.10 \"ls -alhr /var/backups/databases/daily/ --sort=time | grep 'platform' | tail -n 1 | awk '{print \\$9}'\"");
+
+if(empty($filename)) exit('No MySQL dump found!');
+
+// Download the file and extract
+echo "-- Downloading " . $filename . ' archive' . PHP_EOL;
+exec('scp -P 9999 deploy@172.18.150.10:/var/backups/databases/daily/' . $filename . ' ' . $dumps_dir.$filename);
+exec('tar -xvf ' . $filename);
+
+// Now loop over all the databases and import
+echo "-- Importing all databases".PHP_EOL;
+foreach(glob("v2_*.sql") as $file)
+{
+    echo "Importing " . $file . PHP_EOL;
+    $database = substr($file, 0, -4);
+    exec("mysql -u".escapeshellarg($config->user)." -p".escapeshellarg($config->password)." ".$database." < " . $file);
+}
+
+// Now rsync the source files
+echo "-- Syncing shared folders".PHP_EOL;
+exec('rsync --rsh "ssh -p 9999" deploy@172.18.150.10:/var/www/v2.lokalepolitie.be/capistrano/shared/ /var/www/v2.lokalepolitie.be/capistrano/shared/ --update --perms --owner --group --recursive --times --links');
+
+// Get rid of our temporary directories and files
+chdir('/tmp/');
+exec('rm -rf ' . $tmp);
