88131da2c7ede6211abcfb75e9186fc9d7fdfcb5
Tom Janssens <tom@timble.net>
2013-08-26 17:18:22 +0200

    Sync after Nooku Framework update

--- application/admin/component/traffic/view/article/templates/default.html.php
+++ application/admin/component/traffic/view/article/templates/default.html.php
@@ -33,52 +33,6 @@
 		<?= object('com:ckeditor.controller.editor')->render(array('name' => 'text', 'text' => $article->text)) ?>
 	</div>
 	<div class="sidebar">
-        <fieldset>
-            <legend><?= translate('Publish') ?></legend>
-            <div>
-                <label for="published"><?= translate('Published') ?></label>
-                <div>
-                    <input type="checkbox" name="published" value="1" <?= $article->published ? 'checked="checked"' : '' ?> />
-                </div>
-            </div>
-        </fieldset>
-        <fieldset>
-            <legend><?= translate('Details') ?></legend>
-            <div>
-                <label for="date">
-                    <?= translate('Start on') ?>
-                </label>
-                <div>
-                    <?= helper('com:articles.date.datetime', array('row' => $article, 'name' => 'start_on', 'type' => 'date')) ?>
-                </div>
-            </div>
-            <div>
-                <label for="date">
-                    <?= translate('End on') ?>
-                </label>
-                <div>
-                    <?= helper('com:articles.date.datetime', array('row' => $article, 'name' => 'end_on', 'type' => 'date')) ?>
-                </div>
-            </div>
-        </fieldset>
-        <fieldset class="categories group">
-            <legend><?= translate('Category') ?></legend>
-            <div>
-                <?= helper('listbox.radiolist', array(
-                    'list'     => object('com:traffic.model.categories')->sort('title')->table('traffic')->getRowset(),
-                    'selected' => $article->categories_category_id,
-                    'name'     => 'categories_category_id',
-                    'text'     => 'title',
-                ));
-                ?>
-            </div>
-        </fieldset>
-        <? if($article->isStreetable()) : ?>
-        <fieldset>
-            <legend><?= translate('Streets') ?></legend>
-                <?= helper('com:streets.listbox.streets', array('selected' => $article->getStreets()->streets_street_id, 'deselect' => false, 'attribs' => array('multiple' => 'multiple', 'class' => 'select-streets', 'style' => 'width:100%;'))); ?>
-                <script data-inline> $jQuery(".select-streets").select2(); </script>
-        </fieldset>
-        <? endif ?>
+        <?= include('default_sidebar.html') ?>
     </div>
 </form>
