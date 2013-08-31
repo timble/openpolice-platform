acf28ddf7db4f32dfce1c246c60735e3dd6f3b0b
Tom Janssens <tom@timble.net>
2013-08-31 18:18:58 +0200

    Fixed typo #1

--- application/site/component/police/view/page/templates/homepage.html.php
+++ application/site/component/police/view/page/templates/homepage.html.php
@@ -14,7 +14,7 @@
     <div class="span8 sticky alpha">
         <? foreach (object('com:news.model.articles')->sticky(true)->getRowset() as $article) : ?>
             <article>
-                <header>
+                <header class="article__header">
                     <h1><a href="<?= '/'.$site.'/nieuws/'.$article->id.'-'.$article->slug ?>"><?= $article->title ?></a></h1>
                     <span class="timestamp">
                         <?= helper('date.format', array('date'=> $article->ordering_date, 'format' => translate('DATE_FORMAT_LC5'))) ?>
