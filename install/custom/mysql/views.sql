26e37f38b9028597086102f90940d5d0e7f71d75
Tom Janssens <tom@timble.net>
2013-09-05 11:15:16 +0200

    Did some housekeeping on the SQL files #1

--- install/custom/mysql/views.sql
+++
@@ -1,9 +0,0 @@
-DROP VIEW IF EXISTS
-    `questions_view`;
-
-CREATE ALGORITHM = MERGE VIEW `questions_view` AS
-    SELECT
-        `questions`.*
-    FROM
-        `questions` AS `questions`
-   UNION SELECT * from `questions_base`;
