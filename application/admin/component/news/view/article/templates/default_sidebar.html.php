294338f83d8f916e790a0ecf556d58ea92bd537e
Tom Janssens <tom@timble.net>
2013-09-23 17:59:33 +0200

    Added sticky setting to article view #1

---
+++ application/admin/component/news/view/article/templates/default_sidebar.html.php
@@ -0,0 +1,54 @@
+<?
+/**
+ * Belgian Police Web Platform - News Component
+ *
+ * @copyright	Copyright (C) 2012 - 2013 Timble CVBA. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		http://www.police.be
+ */
+?>
+
+<fieldset>
+    <legend><?= translate('Publish') ?></legend>
+    <div>
+        <label for="published"><?= translate('Published') ?></label>
+        <div>
+            <input type="checkbox" name="published" value="1" <?= $article->published ? 'checked="checked"' : '' ?> />
+        </div>
+    </div>
+    <div>
+        <label for="published"><?= translate('Sticky') ?></label>
+        <div>
+            <input type="checkbox" name="sticky" value="1" <?= $article->sticky ? 'checked="checked"' : '' ?> />
+        </div>
+    </div>
+    <div>
+        <label for="created_on"><?= translate('Created on') ?></label>
+        <div class="controls">
+            <?= helper('com:articles.date.datetime', array('row' => $article, 'name' => 'created_on')) ?>
+        </div>
+    </div>
+</fieldset>
+
+<? if($article->isAttachable()) : ?>
+    <fieldset>
+        <legend><?= translate('Attachments') ?></legend>
+        <? if (!$article->isNew()) : ?>
+            <?= import('com:attachments.view.attachments.list.html', array('attachments' => $article->getAttachments(), 'attachments_attachment_id' => $article->attachments_attachment_id)) ?>
+        <? endif ?>
+        <?= import('com:attachments.view.attachments.upload.html') ?>
+    </fieldset>
+<? endif ?>
+
+<? if($article->isTranslatable()) : ?>
+    <fieldset>
+        <legend><?= translate('Translations') ?></legend>
+        <? $translations = $article->getTranslations() ?>
+        <? foreach($article->getLanguages() as $language) : ?>
+            <?= $language->name.':' ?>
+            <? $translation = $translations->find(array('iso_code' => $language->iso_code)) ?>
+            <?= helper('com:languages.grid.status',
+                array('status' => $translation->status, 'original' => $translation->original, 'deleted' => $translation->deleted)) ?>
+        <? endforeach ?>
+    </fieldset>
+<? endif ?>
