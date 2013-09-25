f96dc44aa5dfb691f1ff95efaae9b1df5c364a15
Tom Janssens <tom@timble.net>
2013-09-25 12:15:27 +0200

    Added com:zendesk, plug-and-play integration with Zendesk #1

---
+++ application/admin/component/zendesk/controller/zendesk.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Belgian Police Web Platform - Zendesk Component
+ *
+ * @copyright	Copyright (C) 2012 - 2013 Timble CVBA. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		https://github.com/belgianpolice/internet-platform
+ */
+
+use Nooku\Library;
+
+class ZendeskControllerZendesk extends Library\ControllerView
+{
+    public function _actionRender(Library\CommandContext $context)
+    {
+        $application    = $this->getObject('application');
+        $user           = $this->getUser();
+
+        if($user->getRole() < 24) {
+            $url = clone($context->request->getUrl());
+            $url->query['option'] = 'com_dashboard';
+
+            $application->getRouter()->build($url);
+
+            return $this->getObject('application')->redirect($url);
+        }
+
+        require_once(JPATH_VENDOR . '/firebase/php-jwt/Firebase/PHP-JWT/Authentication/JWT.php');
+
+        $key       = "4DbI0vrQfmQqhZuIAp6NapeI92kEL8CJpb2n4vIT0aGeGiu0";
+        $now       = time();
+
+        $token = array(
+            "jti"   => md5($now . rand()),
+            "iat"   => $now,
+            "name"  => $user->name,
+            "email" => $user->email
+        );
+
+        $jwt = JWT::encode($token, $key);
+
+        $url = 'https://police.zendesk.com/access/jwt?jwt=' . $jwt;
+
+        return $application->redirect($url);
+    }
+}
