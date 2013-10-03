f7439d1ebb3a7c09429aea6f12e89257e2979bc0
Tom Janssens <tom@timble.net>
2013-10-03 11:50:06 +0200

    Fixed template override for com:categories after recent changes to com:attachments #1

--- application/site/public/theme/mobile/templates/categories/categories/default.html.php
+++ application/site/public/theme/mobile/templates/categories/categories/default.html.php
@@ -19,12 +19,6 @@
             </a>
         </h1>
 
-        <? if($category->thumbnail) : ?>
-            <a class="article__thumbnail" href="/<?= $site ?>/contact/je-wijkinspecteur">
-                <img src="" />
-            </a>
-        <? endif ?>
-
         <?= $category->description ?>
 
         <a class="article__readmore" href="/<?= $site ?>/contact/je-wijkinspecteur"><?= translate('Read more') ?></a>
@@ -39,10 +33,12 @@
             </a>
         </h1>
 
-        <? if($category->thumbnail) : ?>
-            <a class="article__thumbnail" href="<?= helper('route.category', array('row' => $category)) ?>">
-                <img src="<?= $category->thumbnail ?>" />
-            </a>
+        <? if($category->attachments_attachment_id) : ?>
+        <a class="article__thumbnail" href="<?= helper('route.category', array('row' => $category)) ?>">
+            <?= helper('com:attachments.image.thumbnail', array(
+                'attachment' => $category->attachments_attachment_id,
+                'attribs' => array('width' => '200', 'align' => 'right'))) ?>
+        </a>
         <? endif ?>
 
         <? if ($category->description) : ?>
