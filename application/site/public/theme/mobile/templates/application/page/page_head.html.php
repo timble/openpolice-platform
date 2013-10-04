67b041f822289bde9b880e68a062777ace70e5d9
Tom Janssens <tom@timble.net>
2013-10-04 12:05:50 +0200

    Added mobile-web-app-capable tag to <head> #1

--- application/site/public/theme/mobile/templates/application/page/page_head.html.php
+++ application/site/public/theme/mobile/templates/application/page/page_head.html.php
@@ -15,6 +15,7 @@
     <meta content="text/html; charset=utf-8" http-equiv="content-type"  />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name=”mobile-web-app-capable” content=”yes”>
 
     <link rel="apple-touch-icon" href="assets://application/images/apple-touch-icon.png"/>
 
