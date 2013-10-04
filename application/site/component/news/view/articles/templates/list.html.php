d6089976c62d74cf0795081a83bccb0892dc5f63
Tom Janssens <tom@timble.net>
2013-10-04 14:40:39 +0200

    Added support for GA 'enhanced link attribution' #1

--- application/site/component/news/view/articles/templates/list.html.php
+++ application/site/component/news/view/articles/templates/list.html.php
@@ -13,14 +13,14 @@
 <? foreach ($articles as $article) : ?>
     <div class="media<?= !$article->attachments_attachment_id ? ' media--imageless' : ''; ?>">
         <? if($article->attachments_attachment_id): ?>
-            <a tabindex="-1" class="pull-left thumbnail media-object" href="<?= '/'.$site.'/nieuws/'.$article->id.'-'.$article->slug ?>">
+            <a id="ga-footer-img-<?= $article->id ?>" tabindex="-1" class="pull-left thumbnail media-object" href="<?= '/'.$site.'/nieuws/'.$article->id.'-'.$article->slug ?>">
                 <?= helper('com:attachments.image.thumbnail', array(
                     'attachment' => $article->attachments_attachment_id,
                     'attribs' => array('width' => '64px'))) ?>
             </a>
         <? endif; ?>
         <div class="media-body">
-            <a class="media-heading" href="<?= '/'.$site.'/nieuws/'.$article->id.'-'.$article->slug ?>"><?= $article->title ?></a>
+            <a id="ga-footer-title-<?= $article->id ?>" class="media-heading" href="<?= '/'.$site.'/nieuws/'.$article->id.'-'.$article->slug ?>"><?= $article->title ?></a>
             <div class="muted" style="font-size: 0.85em">
                 <?= helper('date.format', array('date'=> $article->ordering_date, 'format' => translate('DATE_FORMAT_LC5'))) ?>
             </div>
