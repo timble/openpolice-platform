afda099ab4389898a06c60eb60e9a05a87276a58
Tom Janssens <tom@timble.net>
2013-10-08 16:05:59 +0200

    DOCBLOCKS

--- application/admin/component/about/view/articles/templates/default_scopebar.html.php
+++ application/admin/component/about/view/articles/templates/default_scopebar.html.php
@@ -1,6 +1,6 @@
 <?
 /**
- * Belgian Police Web Platform - Traffic Component
+ * Belgian Police Web Platform - About Component
  *
  * @copyright	Copyright (C) 2012 - 2013 Timble CVBA. (http://www.timble.net)
  * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
