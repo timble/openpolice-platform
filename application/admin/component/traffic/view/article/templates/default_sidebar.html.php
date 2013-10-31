30fad39ad28d1f9e2656e5e07bfea3230cff3ecb
Tom Janssens <tom@timble.net>
2013-10-31 17:03:58 +0100

    Fixed typo re #556

--- application/admin/component/traffic/view/article/templates/default_sidebar.html.php
+++ application/admin/component/traffic/view/article/templates/default_sidebar.html.php
@@ -47,7 +47,7 @@
 <? if($article->isStreetable()) : ?>
 <fieldset>
     <legend><?= translate('Streets') ?></legend>
-    <?= helper('com:streets.listbox.streets', array('selected' => $article->getStreets()->streets_street_id, 'deselect' => false, 'attribs' => array('multiple' => 'multiple', 'class' => 'required select-streets', 'style' => 'width:100%;'))); ?>
+    <?= helper('com:streets.listbox.streets', array('selected' => $article->getStreets()->streets_street_id, 'deselect' => false, 'attribs' => array('multiple' => 'multiple', 'class' => 'select-streets', 'style' => 'width:100%;'))); ?>
     <script data-inline> $jQuery(".select-streets").select2(); </script>
 </fieldset>
 <? endif ?>
