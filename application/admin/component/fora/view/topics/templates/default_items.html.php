e73fefb8041e6f84c38475070a0df6bd756bd9ad
Terry Visser <terry@timble.net>
2013-11-07 13:32:05 +0100

    re #559 added topic item view

---
+++ application/admin/component/fora/view/topics/templates/default_items.html.php
@@ -0,0 +1,32 @@
+<div class="media__item">
+    <div class="media__icon" style="margin-top: 10px">
+        <? if($topic->forum_type != 'article' && ($topic->responded || $topic->status == 'done')) : ?>
+            <i class="icon-ok icon-large color-green"></i>
+        <? else : ?>
+            <i class="icon-<?= $topic->forum_type ?> icon-large"></i>
+        <? endif ?>
+
+        <div class="vote"><i class="icon-thumbs-up"></i> <?= $topic->votes ?></div>
+    </div>
+    <div class="media-body">
+        <h3>
+            <a href="<?= route('view=topic&layout=topic&id='.$topic->id.'&slug='.$topic->slug) ?>">
+                <?= escape($topic->title) ?>
+            </a>
+        </h3>
+
+        <div class="pull-right">
+            <? if(in_array($topic->forum_type, array('issue', 'idea')) && !empty($topic->status)): ?>
+                <span class="label label-<?= $topic->status ?>"><?= $topic->status ?></span>
+            <? endif; ?>
+
+            <? if(!empty($topic->product_version) && $topic->product_version != 'n/a'): ?>
+                <span class="label"><?= $topic->product_version ?></span>
+            <? endif; ?>
+        </div>
+
+        <small class="muted">
+            <strong><?= helper('date.humanize', array('date' => $topic->created_on)) ?></strong> <?= translate('by') ?> <?= escape($topic->created_by_name) ?>
+        </small>
+    </div>
+</div>
