9dc1818d5f2e4487ae1618faf9c0ce0735056b64
Tom Janssens <tom@timble.net>
2013-11-12 18:26:52 +0100

    re #567 - Fixed hardcoded link

--- application/site/component/news/view/articles/templates/list.html.php
+++ application/site/component/news/view/articles/templates/list.html.php
@@ -11,16 +11,17 @@
 <? $site = object('application')->getCfg('site') ?>
 
 <? foreach ($articles as $article) : ?>
+    <? $link = '/'.$site.'/'.object('lib:filter.slug')->sanitize(translate('News')).'/'.$article->id.'-'.$article->slug ?>
     <div class="media<?= !$article->attachments_attachment_id ? ' media--imageless' : ''; ?>">
         <? if($article->attachments_attachment_id): ?>
-            <a tabindex="-1" class="pull-left thumbnail media-object" href="<?= '/'.$site.'/'.translate('news').'/'.$article->id.'-'.$article->slug ?>">
+            <a tabindex="-1" class="pull-left thumbnail media-object" href="<?= $link ?>">
                 <?= helper('com:attachments.image.thumbnail', array(
                     'attachment' => $article->attachments_attachment_id,
                     'attribs' => array('width' => '64px'))) ?>
             </a>
         <? endif; ?>
         <div class="media-body">
-            <a class="media-heading" href="<?= '/'.$site.'/'.translate('news').'/'.$article->id.'-'.$article->slug ?>"><?= $article->title ?></a>
+            <a class="media-heading" href="<?= $link ?>"><?= $article->title ?></a>
             <div class="muted" style="font-size: 0.85em">
                 <?= helper('date.format', array('date'=> $article->ordering_date, 'format' => translate('DATE_FORMAT_LC5'))) ?>
             </div>
