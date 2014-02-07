3b450a7edd593cda1a206167575cdec82b8c3c85
Tom Janssens <tom@timble.net>
2014-02-07 18:39:09 +0100

    re #559 - Restyled attachments

--- application/admin/component/support/view/ticket/templates/default_attachments.html.php
+++ application/admin/component/support/view/ticket/templates/default_attachments.html.php
@@ -2,27 +2,25 @@
 /**
  * Belgian Police Web Platform - Support Component
  *
- * @copyright	Copyright (C) 2012 - 2014 Timble CVBA. (http://www.timble.net)
- * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
- * @link		https://github.com/belgianpolice/internet-platform
+ * @copyright    Copyright (C) 2012 - 2014 Timble CVBA. (http://www.timble.net)
+ * @license        GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        https://github.com/belgianpolice/internet-platform
  */
 ?>
 
 <? $list = (isset($row) && isset($table)) ? $attachments->find(array('row' => $row, 'table' => $table)) : $attachments ?>
 
-<? if(count($list)) : ?>
+<? if (count($list)) : ?>
     <?= helper('behavior.mootools') ?>
     <?= helper('behavior.modal') ?>
 
-    <ul>
-        <? foreach($list as $item) : ?>
-        <li>
-            <? if($item->file->isImage()) : ?>
-            <a class="modal" href="files/<?= $this->getObject('application')->getSite() ?>/attachments/<?= $item->path ?>" rel="{handler: 'image'}"><?= escape($item->name) ?></a>
-            <? else : ?>
-            <a href="<?= route('view=attachment&format=file&id='.$item->id) ?>"><?= escape($item->name) ?></a>
-            <? endif ?>
-        </li>
-        <? endforeach ?>
-    </ul>
+    <? foreach ($list as $item) : ?>
+        <? if ($item->file->isImage()) : ?>
+            <a class="modal attachment"
+               href="files/<?= $this->getObject('application')->getSite() ?>/attachments/<?= $item->path ?>"
+               rel="{handler: 'image'}"><?= escape($item->name) ?></a>
+        <? else : ?>
+            <a class="attachment" href="<?= route('view=attachment&format=file&id=' . $item->id) ?>"><?= escape($item->name) ?></a>
+        <? endif ?>
+    <? endforeach ?>
 <? endif ?>
