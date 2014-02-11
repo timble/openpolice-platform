969b12b1d22f4ad2089d713deedf45a3bf76ac15
Steven Rombauts <steven@timble.net>
2014-02-11 11:30:36 +0100

    Hotfix: make sure 2nd query is run as well in migration file

--- scripts/phpmig/migrations/2014/20140128171714_RenameModuleClasses.php
+++ scripts/phpmig/migrations/2014/20140128171714_RenameModuleClasses.php
@@ -6,7 +6,7 @@ class RenameModuleClasses extends Migration
     public function up()
     {
         $this->_queries = "UPDATE `pages_modules` SET `params` = 'menu_id=\"1\"\nshow_title=\"0\"\nstart_level=\"0\"\nend_level=\"1\"\nshow_children=\"never\"\nclass=\"\"\ncache=\"1\"' WHERE `pages_module_id` = '1';";
-        $this->_queries = "UPDATE `pages_modules` SET `params` = 'menu_id=\"1\"\nshow_title=\"0\"\nstart_level=\"2\"\nend_level=\"0\"\nshow_children=\"active\"\nclass=\"nav nav--list\"\ncache=\"0\"' WHERE `pages_module_id` = '2';";
+        $this->_queries .= "UPDATE `pages_modules` SET `params` = 'menu_id=\"1\"\nshow_title=\"0\"\nstart_level=\"2\"\nend_level=\"0\"\nshow_children=\"active\"\nclass=\"nav nav--list\"\ncache=\"0\"' WHERE `pages_module_id` = '2';";
 
         parent::up();
     }
@@ -14,7 +14,7 @@ class RenameModuleClasses extends Migration
     public function down()
     {
         $this->_queries = "UPDATE `pages_modules` SET `params` = 'menu_id=\"1\"\nshow_title=\"0\"\nstart_level=\"0\"\nend_level=\"1\"\nshow_children=\"never\"\nclass=\"nav\"\ncache=\"1\"' WHERE `pages_module_id` = '1';";
-        $this->_queries = "UPDATE `pages_modules` SET `params` = 'menu_id=\"1\"\nshow_title=\"0\"\nstart_level=\"2\"\nend_level=\"0\"\nshow_children=\"active\"\nclass=\"nav nav-tabs nav-stacked\"\ncache=\"0\"' WHERE `pages_module_id` = '2';";
+        $this->_queries .= "UPDATE `pages_modules` SET `params` = 'menu_id=\"1\"\nshow_title=\"0\"\nstart_level=\"2\"\nend_level=\"0\"\nshow_children=\"active\"\nclass=\"nav nav-tabs nav-stacked\"\ncache=\"0\"' WHERE `pages_module_id` = '2';";
 
         parent::down();
     }
