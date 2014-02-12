13bdfa5cc28e04491b0777ce30c5d23122fb6f76
Tom Janssens <tom@timble.net>
2014-02-12 11:23:16 +0100

    re #622 - Switched to inline CSS

--- application/admin/component/support/view/notification/templates/comment.html.php
+++ application/admin/component/support/view/notification/templates/comment.html.php
@@ -4,35 +4,34 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 
     <title><?= $subject ?></title>
-    <?= import('ticket_style.html'); ?>
 </head>
 <body leftmargin="0" marginwidth="0" topmargin="0" marginheight="0" offset="0">
     <table border="0" cellpadding="0" cellspacing="0" width="100%">
         <tr>
-            <td class="headerContent">
-                <a href="<?= $url ?>">Ticket #<?= $ticket->id ?>: <?= $ticket->title ?></a>
+            <td style="border-collapse: collapse; border-bottom-width: 0; color: #FFF; font-family: Arial; font-weight: bold; line-height: 100%; text-align: center; vertical-align: middle; background-color: #0054ae; padding: 10px 0;" align="center" bgcolor="#0054ae" valign="middle">
+                <a href="<?= $url ?>" style="color: #FFF; font-weight: normal; text-decoration: underline;">Ticket #<?= $ticket->id ?>: <?= $ticket->title ?></a>
             </td>
         </tr>
         <tr>
-            <td valign="top" class="bodyContent">
-                <!-- // Begin Module: Standard Content \\ -->
+            <td valign="top" style="border-collapse: collapse;">
+
                 <table border="0" cellpadding="20" cellspacing="0" width="100%">
                     <tr>
-                        <td valign="top">
-                            <div mc:edit="std_content00">
-                                <p>Ticket <a href="<?= $url ?>">#<?= $ticket->id ?></a> has been updated.</p>
+                        <td valign="top" style="border-collapse: collapse;">
+                            <div mc:edit="std_content00" style="color: #505050; font-family: Arial; font-size: 14px; line-height: 150%; text-align: left;" align="left">
+                                <p>Ticket <a href="<?= $url ?>" style="color: #004d9f; font-weight: normal; text-decoration: underline;">#<?= $ticket->id ?></a> has been updated.</p>
                                 <p>
                                     To review the status of the ticket and add updates, follow the link below:<br />
-                                    <a href="<?= $url ?>"><?= $url ?></a>
+                                    <a href="<?= $url ?>" style="color: #004d9f; font-weight: normal; text-decoration: underline;"><?= $url ?></a>
                                 </p>
-                                <hr />
+                                <hr style="height: 1px; border-top-color: #d3d3d3; margin: 20px 0 0; padding: 20px 0 0; border-style: dotted none none; border-width: 1px 0 0;" />
                                 <? foreach($ticket->getComments() as $comment) : ?>
                                     <strong>
                                         <?= $comment->created_by_name ?> - <?= helper('date.format', array('date'=> $comment->created_on, 'format' => 'd F Y H:i')) ?>
                                     </strong>
                                     <br />
                                     <?= $comment->text ?>
-                                    <hr />
+                                    <hr style="height: 1px; border-top-color: #d3d3d3; margin: 20px 0 0; padding: 20px 0 0; border-style: dotted none none; border-width: 1px 0 0;" />
                                 <? endforeach; ?>
                                 <strong>
                                     <?= $ticket->created_by_name ?> - <?= helper('date.format', array('date'=> $ticket->created_on, 'format' => 'd F Y H:i')) ?>
