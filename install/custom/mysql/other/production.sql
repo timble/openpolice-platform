d6f846e691296851be0884b597a05ef820971745
Tom Janssens <tom@timble.net>
2014-02-25 12:11:24 +0100

    Remove development domains

---
+++ install/custom/mysql/other/production.sql
@@ -0,0 +1,25 @@
+-- Check  for wrong links when publishing a site in production
+UPDATE `about` set `introtext` = replace(`introtext`, 'http://p.pol-nl.be/', '/');
+UPDATE `about` set `introtext` = replace(`introtext`, 'http://p.pol-fr.be/', '/');
+UPDATE `about` set `introtext` = replace(`introtext`, 'http://p.pol-de.be/', '/');
+UPDATE `about` set `fulltext` = replace(`fulltext`, 'http://p.pol-nl.be/', '/');
+UPDATE `about` set `fulltext` = replace(`fulltext`, 'http://p.pol-fr.be/', '/');
+UPDATE `about` set `fulltext` = replace(`fulltext`, 'http://p.pol-de.be/', '/');
+
+UPDATE `contacts` set `misc` = replace(`misc`, 'http://p.pol-nl.be/', '/');
+UPDATE `contacts` set `misc` = replace(`misc`, 'http://p.pol-fr.be/', '/');
+UPDATE `contacts` set `misc` = replace(`misc`, 'http://p.pol-de.be/', '/');
+
+UPDATE `news` set `introtext` = replace(`introtext`, 'http://p.pol-nl.be/', '/');
+UPDATE `news` set `introtext` = replace(`introtext`, 'http://p.pol-fr.be/', '/');
+UPDATE `news` set `introtext` = replace(`introtext`, 'http://p.pol-de.be/', '/');
+UPDATE `news` set `fulltext` = replace(`fulltext`, 'http://p.pol-nl.be/', '/');
+UPDATE `news` set `fulltext` = replace(`fulltext`, 'http://p.pol-fr.be/', '/');
+UPDATE `news` set `fulltext` = replace(`fulltext`, 'http://p.pol-de.be/', '/');
+
+UPDATE `questions` set `text` = replace(`text`, 'http://p.pol-nl.be/', '/');
+UPDATE `questions` set `text` = replace(`text`, 'http://p.pol-fr.be/', '/');
+UPDATE `questions` set `text` = replace(`text`, 'http://p.pol-de.be/', '/');
+UPDATE `traffic` set `text` = replace(`text`, 'http://p.pol-nl.be/', '/');
+UPDATE `traffic` set `text` = replace(`text`, 'http://p.pol-fr.be/', '/');
+UPDATE `traffic` set `text` = replace(`text`, 'http://p.pol-de.be/', '/');
