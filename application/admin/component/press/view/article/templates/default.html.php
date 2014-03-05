f745e791872c4f12dbbbb82e4baa3fc760b9abaa
Tom Janssens <tom@timble.net>
2014-03-05 16:28:13 +0100

    re #655 - Initial import

---
+++ application/admin/component/press/view/article/templates/default.html.php
@@ -0,0 +1,39 @@
+<?
+/**
+ * Belgian Police Web Platform - Press Component
+ *
+ * @copyright	Copyright (C) 2012 - 2013 Timble CVBA. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		https://github.com/belgianpolice/internet-platform
+ */
+?>
+
+<?= helper('behavior.validator'); ?>
+
+<script src="assets://js/koowa.js" />
+<script src="assets://news/js/jquery.datetimepicker.js" />
+<style src="assets://news/css/jquery.datetimepicker.css" />
+
+<ktml:module position="actionbar">
+    <ktml:toolbar type="actionbar">
+</ktml:module>
+
+<form action="" method="post" id="article-form" class="-koowa-form">
+	<input type="hidden" name="published" value="0" />
+    <input type="hidden" name="sticky" value="0" />
+
+	<div class="main">
+		<div class="title">
+			<input class="required" type="text" name="title" maxlength="255" value="<?= $article->title ?>" placeholder="<?= translate('Title') ?>" />
+			<div class="slug">
+			    <span class="add-on">Slug</span>
+			    <input type="text" name="slug" maxlength="255" value="<?= $article->slug ?>" />
+			</div>
+		</div>
+
+		<?= object('com:ckeditor.controller.editor')->render(array('name' => 'text', 'text' => $article->text, 'attribs' => array('class' => 'ckeditor-required'))) ?>
+	</div>
+	<div class="sidebar">
+        <?= import('default_sidebar.html'); ?>
+    </div>
+</form>
---
+++ application/site/component/press/view/articles/rss.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Belgian Police Web Platform - Press Component
+ *
+ * @copyright	Copyright (C) 2012 - 2013 Timble CVBA. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		https://github.com/belgianpolice/internet-platform
+ */
+
+use Nooku\Library;
+
+/**
+ * Articles RSS View
+ *
+ * @author  Tom Janssens <http://nooku.assembla.com/profile/tomjanssens>
+ * @package Component\Press
+ */
+class PressViewArticlesRss extends Library\ViewRss
+{
+    protected function _initialize(Library\ObjectConfig $config)
+    {
+        $config->append(array(
+            'template_filters' => array('com:attachments.template.filter.attachments'),
+        ));
+
+        parent::_initialize($config);
+    }
+
+    public function render()
+    {
+        //Get the parameters
+        $this->params = $this->getObject('application')->getParams();
+
+        // Get the zone
+        $this->zone = $this->getObject('com:police.model.zone')->id($this->getObject('application')->getCfg('site' ))->getRow();
+
+        return parent::render();
+    }
+}
