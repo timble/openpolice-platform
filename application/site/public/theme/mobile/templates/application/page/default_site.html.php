55535c6400ad8e76e36734d39f98c8d7eb689844
Tom Janssens <tom@timble.net>
2014-03-06 15:12:46 +0100

    re #656 - Removed districts search form in footer

--- application/site/public/theme/mobile/templates/application/page/default_site.html.php
+++ application/site/public/theme/mobile/templates/application/page/default_site.html.php
@@ -2,51 +2,6 @@
     $zone = object('com:police.model.zone')->id($site)->getRow();
 ?>
 
-<script>
-    $jQuery(document).ready(function() {
-        function format(item) { return item.title; };
-        $jQuery("#autocomplete__streets--footer").select2({
-            placeholder: "<?= translate('Search your street') ?> ...",
-            minimumInputLength: 3,
-            ajax: { // instead of writing the function to execute the request we use Select2's convenient helper
-                url: "/<?= $site ?>/contact/<?= object('lib:filter.slug')->sanitize(translate('Your district officer')) ?>?view=streets&format=json",
-                dataType: 'json',
-                data: function (term) {
-                    return {
-                        search: term, // search term
-                        page_limit: 10
-                    };
-                },
-                results: function (data) { // parse the results into the format expected by Select2.
-                    // since we are using custom formatting functions we do not need to alter remote JSON data
-                    var results = [];
-                    $jQuery.each(data.items, function(i, item) {
-                        results.push(item.data);
-                    });
-                    return {results: results};
-                }
-            },
-            initSelection: function(element, callback) {
-                // the input tag has a value attribute preloaded that points to a preselected movie's id
-                // this function resolves that id attribute to an object that select2 can render
-                // using its formatResult renderer - that way the movie name is shown preselected
-                var id=$jQuery(element).val();
-                if (id!=="") {
-                    $jQuery.ajax("/<?= $site ?>/contact/<?= object('lib:filter.slug')->sanitize(translate('Your district officer')) ?>?view=street&format=json&id="+id, {
-                        dataType: "json"
-                    }).done(function(data) { callback(data.item); });
-                }
-            },
-            formatResult: format, // omitted for brevity, see the source of this page
-            formatSelection: format, // omitted for brevity, see the source of this page
-            dropdownCssClass: "bigdrop", // apply css that makes the dropdown taller
-            formatInputTooShort: false,
-            formatSearching: function () { return "<?= translate('Please wait') ?> ..."; },
-            formatNoMatches: function () { return "<?= translate('No matches found') ?>"; }
-        });
-    });
-</script>
-
 <div id="wrap">
     <div class="container container__header">
         <div class="organization" itemscope itemtype="http://schema.org/Organization">
@@ -109,14 +64,8 @@
             </div>
             <div class="footer__districts">
                 <h3><?= translate('Your district officer') ?></h3>
-                <form action="/<?= $site ?>/contact/<?= object('lib:filter.slug')->sanitize(translate('Your district officer')) ?>" method="get" class="-koowa-form">
-                    <div class="control-group">
-                        <div class="controls">
-                            <input type="text" class="bigdrop" id="autocomplete__streets--footer" placeholder="<?= translate('Search your street') ?> ..." name="street" value="<?= @$_COOKIE ['district_street'] ?>">
-                        </div>
-                    </div>
-                    <button class="button button--primary"><?= translate('Search') ?></button>
-                </form>
+                <p>Ken je de verantwoordelijke wijkinspecteur in je buurt? Hij of zij is je eerste contactpersoon bij de politie.</p>
+                <a href="/<?= $site ?>/contact/<?= object('lib:filter.slug')->sanitize(translate('Your district officer')) ?>"><?= translate('Contact your district officer') ?>.</a>
             </div>
         </div>
     </div>
