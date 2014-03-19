26ccdabe86ca11083330634201f96d363a5d9b1f
Tom Janssens <tom@timble.net>
2014-03-19 11:47:44 +0100

    re #663 - Let's give the homepage contact information some color on mobile

--- application/site/component/police/view/page/templates/homepage.html.php
+++ application/site/component/police/view/page/templates/homepage.html.php
@@ -49,16 +49,16 @@
             <h3><?= translate('Contact us') ?></h3>
             <div  class="well well--small">
                 <div>
-                    <span><?= translate('Urgent police assistance') ?></span><br />
+                    <span class="muted"><?= translate('Urgent police assistance') ?></span><br />
                     <span class="text--strong"><a tabindex="-1" href="tel:101">101</a></span>
                 </div>
                 <div>
-                    <span><?= translate('No emergency') ?></span><br />
+                    <span class="muted"><?= translate('No emergency') ?></span><br />
                     <span class="text--strong"><a tabindex="-1" href="tel:<?= str_replace(' ', '', $zone->phone_emergency) ?>"><?= $zone->phone_emergency ?></a></span>
                 </div>
                 <? if($zone->phone_information) : ?>
                     <div>
-                        <span><?= translate('General information') ?></span><br />
+                        <span class="muted"><?= translate('General information') ?></span><br />
                         <span class="text--strong"><a tabindex="-1" href="tel:<?= str_replace(' ', '', $zone->phone_information) ?>"><?= $zone->phone_information ?></a></span>
                     </div>
                 <? endif ?>
