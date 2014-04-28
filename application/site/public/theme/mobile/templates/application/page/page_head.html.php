e7d6b7b7d1f4eb96eb9386a2a5e2fccafca2fd00
Tom Janssens <tom@timble.net>
2014-04-28 16:55:41 +0200

    re #699 - Update to Universal Tracking code

--- application/site/public/theme/mobile/templates/application/page/page_head.html.php
+++ application/site/public/theme/mobile/templates/application/page/page_head.html.php
@@ -37,15 +37,15 @@
     <script src="assets://application/js/placeholder.js" condition="if lte IE 9" />
 
     <?php if($site && $analytics = object('application')->getCfg('analytics')) : ?>
-        <script>
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-            ga('create', 'UA-20242887-1', 'lokalepolitie.be');
-            ga('send', 'pageview');
+        ga('create', 'UA-20242887-1', 'lokalepolitie.be');
+        ga('send', 'pageview');
 
-        </script>
+    </script>
     <?php endif; ?>
 </head>
