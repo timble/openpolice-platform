c2848eadcd339ef5a41fbde669864f2ba996a32e
Steven Rombauts <steven@timble.net>
2014-04-29 15:33:20 +0200

    re #700 - Move to Slack

---
+++ component/slack/controller/message.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * Belgian Police Web Platform - Slack Component
+ *
+ * @copyright	Copyright (C) 2012 - 2014 Timble CVBA. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		https://github.com/belgianpolice/internet-platform
+ */
+
+namespace Nooku\Component\Slack;
+
+use Nooku\Library;
+
+class ControllerMessage extends Library\ControllerAbstract
+{
+    const YELLOW = '#F0E771';
+    const RED    = '#E32929';
+    const PURPLE = '#C036E3';
+    const GRAY   = '#CAC4CC';
+    const GREEN  = '#5AE622';
+
+    protected $_token;
+    protected $_channel;
+    protected $_from;
+
+    public function __construct(Library\ObjectConfig $config)
+    {
+        parent::__construct($config);
+
+        $this->_token   = $config->token;
+        $this->_channel = $config->channel;
+        $this->_from    = $config->from;
+
+        $this->_emoji = $config->icon_emoji;
+        $this->_icon  = $config->icon_url;
+    }
+
+    protected function _initialize(Library\ObjectConfig $config)
+    {
+        $application = $this->getObject('application');
+
+        $config->append(array(
+            'channel'    => '#police-support',
+            'from'       => 'Wilfried Pasmans',
+            'token'      => $application->getCfg('slack_token'),
+            'icon_emoji' => ':cop:',
+            'icon_url'   => ''
+        ));
+
+        parent::_initialize($config);
+    }
+
+    protected function _actionSend(Library\CommandContext $context)
+    {
+        $data = $context->request->data;
+
+        if(empty($this->_token)) {
+            return false;
+        }
+
+        $payload = new \stdClass;
+        $payload->username = $this->_from;
+        $payload->text     = $data->message;
+
+        if (!empty($this->_channel)) {
+            $payload->channel = $this->_channel;
+        }
+
+        if (!empty($this->_icon)) {
+            $payload->icon_url = $this->_icon;
+        } elseif (!empty($this->_emoji)) {
+            $payload->icon_emoji = $this->_emoji;
+        }
+
+        if (is_array($data->attachments) && count($data->attachments)) {
+            $payload->attachments = $data->attachments;
+        }
+
+        $json = json_encode($payload);
+
+        return $this->_execCurl($json);
+    }
+
+    protected function _execCurl($json)
+    {
+        $application = $this->getObject('application');
+
+        if (!function_exists('curl_init')) {
+            throw new \RuntimeException('Curl library does not exist');
+        }
+
+        $url = 'https://timble.slack.com/services/hooks/incoming-webhook?token=' . $this->_token;
+
+        $ch = curl_init();
+
+        curl_setopt_array($ch, array(
+            CURLOPT_URL            => $url,
+            CURLOPT_SSL_VERIFYPEER => 0,
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_FOLLOWLOCATION => 1,
+            CURLOPT_MAXREDIRS      => 10,
+            CURLOPT_TIMEOUT        => 15,
+            CURLOPT_POST           => 1,
+            CURLOPT_HTTPHEADER     => array('Content-Type: text/plain'),
+            CURLOPT_POSTFIELDS     => $json
+        ));
+
+        if($proxy = $application->getCfg('http_proxy')) {
+            curl_setopt($ch, CURLOPT_PROXY, $proxy);
+        }
+
+        $response = curl_exec($ch);
+
+        if(curl_errno($ch)) {
+            throw new \RuntimeException('Curl Error: '.curl_error($ch));
+        }
+
+        curl_close($ch);
+
+        if($response != 'ok') {
+            throw new \RuntimeException('Slack failed to post the message: ' . $response);
+        }
+
+        return true;
+    }
+}
--- component/support/controller/behavior/broadcastable.php
+++ component/support/controller/behavior/broadcastable.php
@@ -10,22 +10,90 @@
 namespace Nooku\Component\Support;
 
 use Nooku\Library;
-use Nooku\Component\Hipchat;
+use Nooku\Component\Slack;
 
-class ControllerBehaviorBroadcastable extends Hipchat\ControllerBehaviorBroadcastable
+class ControllerBehaviorBroadcastable extends Slack\ControllerBehaviorBroadcastable
 {
-    protected function _getHeading(Library\DatabaseRowAbstract $entity)
+    protected function _getMessage(Library\DatabaseRowAbstract $entity)
     {
         $name = $entity->getIdentifier()->name;
 
         $user = $this->getObject('user');
         $ticket = $name == 'ticket' ? $entity : $this->getObject('com:support.model.tickets')->id($entity->row)->getRow();
 
+        $url  = $this->_getTicketURL($ticket->id);
+        $link = '<'.$url.'|' . $ticket->title . '>';
+
+        if ($name == 'ticket') {
+            $message = 'New *ticket* ' . $link . ' created by _' . $user->getName().'_';
+        } else {
+            $message = 'New *'.$name.'* to ' . $link . ' by _' . $user->getName().'_';
+        }
+
+        return $message;
+    }
+
+    protected function _getAttachments(Library\DatabaseRowAbstract $entity)
+    {
+        $attachment = new \stdClass;
+
+        $name   = $entity->getIdentifier()->name;
+        $ticket = $name == 'ticket' ? $entity : $this->getObject('com:support.model.tickets')->id($entity->row)->getRow();
+        $url    = $this->_getTicketURL($ticket->id);
+
+        $attachment->fallback = 'Police support request <'.$url.'|' . $ticket->title . '> (#' . $ticket->id.')';
+        $attachment->color    = $this->_getColor($entity);
+
+        $fields = array();
+
+        $status = new \stdClass;
+        $status->title = \JText::_('Status');
+        $status->value = \JText::_(ucfirst($ticket->status));
+        $status->short = true;
+
+        $fields[] = $status;
+
+        $author = new \stdClass;
+        $author->title = \JText::_('Posted by');
+        $author->value = $this->getObject('user')->getName();
+        $author->short = true;
+
+        $fields[] = $author;
+
+        $message = new \stdClass;
+        $message->title = \JText::_('Message');
+        $message->value = strip_tags($entity->text);
+        $message->short = false;
+
+        $fields[] = $message;
+
+        $attachment->fields = $fields;
+
+        return array($attachment);
+    }
+
+    protected function _getColor(Library\DatabaseRowAbstract $entity)
+    {
+        $request = $this->getMixer()->getRequest();
+        $name    = $entity->getIdentifier()->name;
+
+        if($name == 'ticket') {
+            return Slack\ControllerMessage::RED;
+        }
+        elseif($name == 'comment' && $request->data->status == 'solved') {
+            return Slack\ControllerMessage::GREEN;
+        }
+
+        return Slack\ControllerMessage::YELLOW;
+    }
+
+    protected function _getTicketURL($id)
+    {
         // Create the route to the topic
         $parts = array(
             'view'   => 'ticket',
             'option' => 'com_support',
-            'id'     => ($name == 'ticket' ? $entity->id : $entity->row)
+            'id'     => $id
         );
 
         $host = $this->getObject('request')->getBaseUrl()->toString(Library\HttpUrl::SCHEME | Library\HttpUrl::HOST);
@@ -35,23 +103,7 @@ class ControllerBehaviorBroadcastable extends Hipchat\ControllerBehaviorBroadcas
         ));
 
         $url  = $host.$path;
-        $link = '<a href="'.$url.'">' . $ticket->title . '</a>';
 
-        return '<strong>' . $user->getName() . ' - ' . $link . '</strong>';;
-    }
-
-    protected function _getBackgroundColor(Library\DatabaseRowAbstract $entity)
-    {
-        $request = $this->getMixer()->getRequest();
-        $name    = $entity->getIdentifier()->name;
-
-        if($name == 'ticket') {
-            return Hipchat\ControllerMessage::RED;
-        }
-        elseif($name == 'comment' && $request->data->status == 'solved') {
-            return Hipchat\ControllerMessage::GREEN;
-        }
-
-        return Hipchat\ControllerMessage::YELLOW;
+        return $url;
     }
 }
