ab81486d640498536c9a470778e78a00f84fc3fe
Tom Janssens <tom@timble.net>
2014-05-27 16:19:02 +0200

    re #720 - Render the blog's JSON feed in the Dashboard

--- application/admin/public/theme/default/templates/dashboard/dashboard/default.html.php
+++ application/admin/public/theme/default/templates/dashboard/dashboard/default.html.php
@@ -2,5 +2,25 @@
 
 <div class="sidebar">
     <h3><?= translate('Announcements'); ?></h3>
-    <?= object('com:support.controller.announcement')->layout('table')->limit(10)->render(); ?>
+
+    <?
+    $contents = file_get_contents('http://belgianpolice.github.io/blog.json');
+    $contents = utf8_encode($contents);
+    $posts = json_decode($contents);
+    ?>
+
+    <table class="table">
+        <? foreach($posts as $post) : ?>
+            <tr>
+                <td>
+                    <a target="_blank" href="<?= $post->url ?>">
+                        <?= $post->title ?>
+                    </a>
+                </td>
+                <td>
+                    <?= helper('date.humanize', array('date' => $post->date)) ?>
+                </td>
+            </tr>
+        <? endforeach ?>
+    </table>
 </div>
