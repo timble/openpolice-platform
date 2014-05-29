8782823c9beb47754f996e2af808743c1c42e6bc
Tom Janssens <tom@timble.net>
2014-05-29 13:43:35 +0200

    re #721 - Tracking was not working cross domain

--- application/admin/public/theme/default/templates/application/page/page_head.html.php
+++ application/admin/public/theme/default/templates/application/page/page_head.html.php
@@ -42,7 +42,7 @@
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-    ga('create', 'UA-20242887-3', 'lokalepolitie.be');
+    ga('create', 'UA-20242887-3', 'auto');
     ga('send', 'pageview');
 
     </script>
