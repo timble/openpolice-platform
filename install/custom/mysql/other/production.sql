2ab7e99684f494ff9ef4be7b976bba7e4bd6a0cf
Tom Janssens <tom@timble.net>
2014-06-12 15:54:46 +0200

    Check for wrong links when publishing a site in production

--- install/custom/mysql/other/production.sql
+++ install/custom/mysql/other/production.sql
@@ -6,6 +6,10 @@ UPDATE `about` set `fulltext` = replace(`fulltext`, 'http://p.pol-nl.be/', '/');
 UPDATE `about` set `fulltext` = replace(`fulltext`, 'http://p.pol-fr.be/', '/');
 UPDATE `about` set `fulltext` = replace(`fulltext`, 'http://p.pol-de.be/', '/');
 
+UPDATE `categories` set `description` = replace(`description`, 'http://p.pol-nl.be/', '/');
+UPDATE `categories` set `description` = replace(`description`, 'http://p.pol-fr.be/', '/');
+UPDATE `categories` set `description` = replace(`description`, 'http://p.pol-de.be/', '/');
+
 UPDATE `contacts` set `misc` = replace(`misc`, 'http://p.pol-nl.be/', '/');
 UPDATE `contacts` set `misc` = replace(`misc`, 'http://p.pol-fr.be/', '/');
 UPDATE `contacts` set `misc` = replace(`misc`, 'http://p.pol-de.be/', '/');
@@ -17,9 +21,14 @@ UPDATE `news` set `fulltext` = replace(`fulltext`, 'http://p.pol-nl.be/', '/');
 UPDATE `news` set `fulltext` = replace(`fulltext`, 'http://p.pol-fr.be/', '/');
 UPDATE `news` set `fulltext` = replace(`fulltext`, 'http://p.pol-de.be/', '/');
 
+UPDATE `press` set `text` = replace(`text`, 'http://p.pol-nl.be/', '/');
+UPDATE `press` set `text` = replace(`text`, 'http://p.pol-fr.be/', '/');
+UPDATE `press` set `text` = replace(`text`, 'http://p.pol-de.be/', '/');
+
 UPDATE `questions` set `text` = replace(`text`, 'http://p.pol-nl.be/', '/');
 UPDATE `questions` set `text` = replace(`text`, 'http://p.pol-fr.be/', '/');
 UPDATE `questions` set `text` = replace(`text`, 'http://p.pol-de.be/', '/');
+
 UPDATE `traffic` set `text` = replace(`text`, 'http://p.pol-nl.be/', '/');
 UPDATE `traffic` set `text` = replace(`text`, 'http://p.pol-fr.be/', '/');
 UPDATE `traffic` set `text` = replace(`text`, 'http://p.pol-de.be/', '/');
