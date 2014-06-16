0d8d74bc8a82c8a89772efbae7258d3008935b72
Tom Janssens <tom@timble.net>
2014-06-16 11:26:36 +0200

    re #743 - Do not take away user-scalable functionality from the user

--- application/site/public/theme/mobile/templates/application/page/page_head.html.php
+++ application/site/public/theme/mobile/templates/application/page/page_head.html.php
@@ -13,7 +13,7 @@
     <title><?= title() ?></title>
 
     <meta content="text/html; charset=utf-8" http-equiv="content-type"  />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <meta name=”mobile-web-app-capable” content=”yes” />
 
