d9cf180618d54629002da944c7c0a8b2bcf2c819
Steven Rombauts <steven@timble.net>
2014-06-19 16:21:34 +0200

    re #692 - Clean-up

--- application/manager/component/support/database/rowset/tickets.php
+++ application/manager/component/support/database/rowset/tickets.php
@@ -11,33 +11,4 @@ class SupportDatabaseRowsetTickets extends Library\DatabaseRowsetAbstract
 
         parent::_initialize($config);
     }
-
-
-    public function addRow(array $rows, $status = NULL)
-    {
-        if ($this->_row_cloning)
-        {
-            $default = $this->getRow()->setStatus($status);
-
-            foreach ($rows as $k => $data)
-            {
-                $row = clone $default;
-                $row->setData($data, $row->isNew());
-
-                $this->insert($row);
-            }
-        }
-        else
-        {
-            foreach ($rows as $k => $data)
-            {
-                $row = $this->getRow()->setStatus($status);
-                $row->setData($data, $row->isNew());
-
-                $this->insert($row);
-            }
-        }
-
-        return $this;
-    }
 }
