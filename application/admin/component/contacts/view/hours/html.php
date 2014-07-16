4b5ca2a874e23e94cfb4924c4cff587ed0646350
Tom Janssens <tom@timble.net>
2014-07-16 17:55:51 +0200

    HOTFIX - Categories are decoupled now

--- application/admin/component/contacts/view/hours/html.php
+++ application/admin/component/contacts/view/hours/html.php
@@ -14,7 +14,7 @@ class ContactsViewHoursHtml extends Library\ViewHtml
     public function render()
     {
         $this->contacts = $this->getObject('com:contacts.model.contacts')->sort('title')->getRowset();
-        $this->categories = $this->getObject('com:contacts.model.categories')->table('contacts')->sort('title')->getRowset();
+        $this->categories = $this->getObject('com:contacts.model.categories')->sort('title')->getRowset();
 
         return parent::render();
     }
