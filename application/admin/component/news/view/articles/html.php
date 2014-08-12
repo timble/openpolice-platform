f04698b77f5aafe9748f4e55e98d2fb8eed6ab01
Tom Janssens <tom@timble.net>
2014-08-12 15:59:42 +0200

    re #787 - Improved UI

---
+++ application/admin/component/news/view/articles/html.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Belgian Police Web Platform - News Component
+ *
+ * @copyright	Copyright (C) 2012 - 2013 Timble CVBA. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		https://github.com/belgianpolice/internet-platform
+ */
+
+use Nooku\Library;
+
+class NewsViewArticlesHtml extends Library\ViewHtml
+{
+    public function render()
+    {
+        $date = new Library\Date(array('timezone' => 'GMT'));
+
+        $this->now = $date->getTimestamp();
+
+        return parent::render();
+    }
+}
