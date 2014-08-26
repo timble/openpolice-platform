a5856dada0720497b7fd6d7b126138f1ef84ebf1
Steven Rombauts <steven@timble.net>
2014-08-26 13:02:21 +0200

    re #782 - Turn logic around, get our local list of cities and compare to results from CRAB

---
+++ component/streets/database/rowset/soap.php
@@ -0,0 +1,9 @@
+<?php
+namespace Nooku\Component\Streets;
+
+use Nooku\Library;
+
+class DatabaseRowsetSoap extends Library\DatabaseRowsetAbstract
+{
+
+}
--- scripts/crons/crab.php
+++ scripts/crons/crab.php
@@ -3,13 +3,9 @@ require_once '_bootstrap.php';
 
 $client = new SoapClient('http://crab.agiv.be/wscrab/WsCrab.svc?wsdl');
 
-$regions = array(1, 2, 3);
-
-$query  = $manager->getObject('lib:database.query.select');
-$cities = $manager->getObject('com:streets.database.table.cities')->select($query);
-
 // For every region, request the list of all cities
-foreach ($regions as $region)
+$crab_cities = $manager->getObject('com:streets.database.rowset.soap');
+foreach (array(1, 2, 3) as $region)
 {
     $requestParams = array(
         'SorteerVeld' => 0,
@@ -18,18 +14,29 @@ foreach ($regions as $region)
 
     $results = $client->ListGemeentenByGewestId($requestParams);
 
-    foreach ($results->ListGemeentenByGewestIdResult->GemeenteItem as $result)
+    $crab_cities->addRow((array) $results->ListGemeentenByGewestIdResult->GemeenteItem);
+}
+
+$query  = $manager->getObject('lib:database.query.select');
+$cities = $manager->getObject('com:streets.database.table.cities')->select($query);
+
+foreach ($cities as $city)
+{
+    $crab_city = $crab_cities->find(array('NISGemeenteCode' => $city->id, 'TaalCode' => $city->language))->top();
+
+    if (!$crab_city->GemeenteId)
     {
-        $city = $cities->find(array('id' => $result->NISGemeenteCode))->top();
+        trigger_error($city->title . ' not found in CRAB database! (#' . $city->id . ')');
+        continue;
+    }
 
-        if ($city->isNew())
-        {
-            trigger_error($result->GemeenteNaam . ' does not exist in data.streets_cities! (NIS: ' . $result->NISGemeenteCode . ')');
-            continue;
-        }
 
-        if (!$city->crab_city_id) {
-            $city->setData(array('crab_city_id' => $result->GemeenteId))->save();
-        }
+    if ($city->crab_city_id != $crab_city->GemeenteId || $city->title != $crab_city->GemeenteNaam)
+    {
+        $city->setData(array(
+                'crab_city_id' => $crab_city->GemeenteId,
+                'title'        => $crab_city->GemeenteNaam
+            ))
+            ->save();
     }
 }
