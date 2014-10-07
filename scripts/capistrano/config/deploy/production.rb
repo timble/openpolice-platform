860af83ba94c43cf312ef3e9a29048230aa53034
Steven Rombauts <steven@timble.net>
2014-10-07 20:18:58 +0200

    Remove New Relic

--- scripts/capistrano/config/deploy/production.rb
+++ scripts/capistrano/config/deploy/production.rb
@@ -4,6 +4,3 @@ server "police-production", :app, :web, :db, :primary => true
 
 set :rails_env, "production"
 set :branch, "master"
-
-after "deploy:update", "newrelic:notice_deployment"
-after "deploy:migrate", "newrelic:notice_deployment"
