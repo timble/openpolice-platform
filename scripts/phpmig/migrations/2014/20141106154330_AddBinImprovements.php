37a5e4b338c744077331a7af95a36db6c866d041
Tom Janssens <tom@timble.net>
2014-11-19 17:24:16 +0100

    re #878 - Update links

--- scripts/phpmig/migrations/2014/20141106154330_AddBinImprovements.php
+++ scripts/phpmig/migrations/2014/20141106154330_AddBinImprovements.php
@@ -13,6 +13,11 @@ class AddBinImprovements extends Migration
         $this->_queries .= "UPDATE `pages` SET `title` = 'Je wijk', `slug` = 'je-wijk' WHERE `pages_page_id` = '43';";
         $this->_queries .= "UPDATE `contacts_categories` SET `title` = 'Je wijk', `slug` = 'je-wijk' WHERE `contacts_category_id` = '24';";
 
+        $this->_queries .= "UPDATE `about` set `introtext` = replace(`introtext`, '/contact/je-wijkinspecteur', '/contact/je-wijk');";
+        $this->_queries .= "UPDATE `about` set `fulltext` = replace(`fulltext`, '/contact/je-wijkinspecteur', '/contact/je-wijk');";
+        $this->_queries .= "UPDATE `contacts` set `misc` = replace(`misc`, '/contact/je-wijkinspecteur', '/contact/je-wijk');";
+        $this->_queries .= "UPDATE `about_categories`, `contacts_categories`, `questions`, `questions_categories` set `description` = replace(`description`, '/contact/je-wijkinspecteur', '/contact/je-wijk');";
+
         parent::up();
 
         // All the French speaking zones.
@@ -20,6 +25,11 @@ class AddBinImprovements extends Migration
 
         $this->_queries = "UPDATE `pages` SET `title` = 'Votre quartier', `slug` = 'votre-quartier' WHERE `pages_page_id` = '43';";
         $this->_queries .= "UPDATE `contacts_categories` SET `title` = 'Votre quartier', `slug` = 'votre-quartier' WHERE `contacts_category_id` = '24';";
+
+        $this->_queries .= "UPDATE `about` set `introtext` = replace(`introtext`, '/contact/votre-agent-de-quartier', '/contact/votre-quartier');";
+        $this->_queries .= "UPDATE `about` set `fulltext` = replace(`fulltext`, '/contact/votre-agent-de-quartier', '/contact/votre-quartier');";
+        $this->_queries .= "UPDATE `contacts` set `misc` = replace(`misc`, '/contact/votre-agent-de-quartier', '/contact/votre-quartier');";
+        $this->_queries .= "UPDATE `about_categories`, `contacts_categories`, `questions`, `questions_categories` set `description` = replace(`description`, '/contact/votre-agent-de-quartier', '/contact/votre-quartier');";
 
         parent::up();
 
@@ -39,6 +49,11 @@ class AddBinImprovements extends Migration
     {
         $this->_queries = "UPDATE `pages` SET `title` = 'Je wijkinspecteur', `slug` = 'je-wijkinspecteur' WHERE `pages_page_id` = '43';";
 
+        $this->_queries .= "UPDATE `about` set `introtext` = replace(`introtext`, '/contact/je-wijk', '/contact/je-wijkinspecteur');";
+        $this->_queries .= "UPDATE `about` set `fulltext` = replace(`fulltext`, '/contact/je-wijk', '/contact/je-wijkinspecteur');";
+        $this->_queries .= "UPDATE `contacts` set `misc` = replace(`misc`, '/contact/je-wijk', '/contact/je-wijkinspecteur');";
+        $this->_queries .= "UPDATE `about_categories`, `contacts_categories`, `questions`, `questions_categories` set `description` = replace(`description`, '/contact/je-wijk', '/contact/je-wijkinspecteur');";
+
         parent::down();
 
         // All the French speaking zones.
@@ -46,6 +61,11 @@ class AddBinImprovements extends Migration
 
         $this->_queries = "UPDATE `pages` SET `title` = 'Votre agent de quartier', `slug` = 'votre-agent-de-quartier' WHERE `pages_page_id` = '43';";
         $this->_queries .= "UPDATE `contacts_categories` SET `title` = 'Votre quartier', `slug` = 'votre-quartier' WHERE `contacts_category_id` = '24';";
+
+        $this->_queries .= "UPDATE `about` set `introtext` = replace(`introtext`, '/contact/votre-quartier', '/contact/votre-agent-de-quartier');";
+        $this->_queries .= "UPDATE `about` set `fulltext` = replace(`fulltext`, '/contact/votre-quartier', '/contact/votre-agent-de-quartier');";
+        $this->_queries .= "UPDATE `contacts` set `misc` = replace(`misc`, '/contact/votre-quartier', '/contact/votre-agent-de-quartier');";
+        $this->_queries .= "UPDATE `about_categories`, `contacts_categories`, `questions`, `questions_categories` set `description` = replace(`description`, '/contact/votre-quartier', '/contact/votre-agent-de-quartier');";
 
         parent::down();
 
