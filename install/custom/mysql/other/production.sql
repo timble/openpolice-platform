5c3d1940a846c53db4f06159023c58107188ac98
Tom Janssens <tom@timble.net>
2014-11-28 16:11:07 +0100

    Separate SQL code

--- install/custom/mysql/other/production.sql
+++ install/custom/mysql/other/production.sql
@@ -109,18 +109,3 @@ UPDATE `districts_officers` set `phone` = replace(`phone`, '/', ' ');
 UPDATE `districts_officers` set `phone` = replace(`phone`, '.', ' ');
 UPDATE `districts_officers` set `phone` = replace(`phone`, '   ', ' ');
 UPDATE `districts_officers` set `phone` = replace(`phone`, '  ', ' ');
-
-
--- Find and Replace
-UPDATE `about` set `introtext` = replace(`introtext`, 'find', 'replace');
-UPDATE `about_categories` set `description` = replace(`description`, 'find', 'replace');
-UPDATE `categories` set `description` = replace(`description`, 'find', 'replace');
-UPDATE `contacts` set `misc` = replace(`misc`, 'find', 'replace');
-UPDATE `contacts_categories` set `description` = replace(`description`, 'find', 'replace');
-UPDATE `news` set `introtext` = replace(`introtext`, 'find', 'replace');
-UPDATE `news` set `fulltext` = replace(`fulltext`, 'find', 'replace');
-UPDATE `press` set `text` = replace(`text`, 'find', 'replace');
-UPDATE `questions` set `text` = replace(`text`, 'find', 'replace');
-UPDATE `questions_categories` set `description` = replace(`description`, 'find', 'replace');
-UPDATE `traffic` set `text` = replace(`text`, 'find', 'replace');
-UPDATE `traffic_categories` set `description` = replace(`description`, 'find', 'replace');
