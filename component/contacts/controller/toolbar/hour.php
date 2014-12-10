2a4c005657f63a45f6d86fafd347d114b934d347
Tom Janssens <tom@timble.net>
2014-12-10 11:15:51 +0100

    re #919 - Added exceptions to office hours and automatically select the top contact as default

---
+++ component/contacts/controller/toolbar/hour.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Belgian Police Web Platform - Contacts Component
+ *
+ * @copyright	Copyright (C) 2012 - 2013 Timble CVBA. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		https://github.com/belgianpolice/internet-platform
+ */
+
+namespace Nooku\Component\Contacts;
+
+use Nooku\Library;
+
+class ControllerToolbarHour extends Library\ControllerToolbarActionbar
+{
+    protected function _afterControllerBrowse(Library\CommandContext $context)
+    {
+        parent::_afterControllerBrowse($context);
+
+
+    }
+
+    protected function _commandNew(Library\ControllerToolbarCommand $command)
+    {
+        $contact  = $this->getController()->getModel()->getState()->contact;
+        $command->href .= '&contact='.$contact;
+    }
+}
