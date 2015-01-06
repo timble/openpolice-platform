2c050641aae5aee9e3f70cff5d89c5bf85539673
Tom Janssens <tom@timble.net>
2015-01-06 14:17:01 +0100

    #26 - Use streets identifier instead of id column

--- application/admin/component/traffic/view/article/templates/default_sidebar.html.php
+++ application/admin/component/traffic/view/article/templates/default_sidebar.html.php
@@ -73,7 +73,7 @@
 <? if($article->isLocatable()) : ?>
 <fieldset>
     <legend><?= translate('Streets') ?></legend>
-    <?= helper('com:streets.listbox.streets', array('selected' => $article->getStreets()->id, 'deselect' => false, 'attribs' => array('multiple' => 'multiple', 'class' => 'select-streets', 'style' => 'width:100%;'))); ?>
+    <?= helper('com:streets.listbox.streets', array('selected' => $article->getStreets()->streets_street_identifier, 'deselect' => false, 'attribs' => array('multiple' => 'multiple', 'class' => 'select-streets', 'style' => 'width:100%;'))); ?>
     <script data-inline> $jQuery(".select-streets").select2(); </script>
 </fieldset>
 <? endif ?>
