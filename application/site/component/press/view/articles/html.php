11f5295819d1ff35e6f77fd9e4669e92321fc78c
Tom Janssens <tom@timble.net>
2015-03-20 17:54:45 +0100

    #120 - Replaced hardcoded contacts_contact_id to a parameter

---
+++ application/site/component/press/view/articles/html.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Belgian Police Web Platform - Traffic Component
+ *
+ * @copyright	Copyright (C) 2012 - 2013 Timble CVBA. (http://www.timble.net)
+ * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link		https://github.com/belgianpolice/internet-platform
+ */
+
+use Nooku\Library;
+
+class PressViewArticlesHtml extends Library\ViewHtml
+{
+    public function render()
+    {
+        //Get the parameters
+        $this->params = $this->getObject('application')->getParams();
+
+        return parent::render();
+    }
+}
