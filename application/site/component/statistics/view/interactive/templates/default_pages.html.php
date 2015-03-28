c44e2ac7e06dc01505c6fdaffeed1bace68dee38
Tom Janssens <tom@timble.net>
2015-03-28 15:40:31 +0100

    #132 - Added interactive statistics

---
+++ application/site/component/statistics/view/interactive/templates/default_pages.html.php
@@ -0,0 +1,36 @@
+<?
+    $graphs = array(
+        'Overview' => array(
+            'By criminal figure' => 'pcs_dashboard_crimfig_misdrijven',
+            'By police zone' => 'pcs_dashboard_pz_crimfig_misdrijven'
+        ),
+        'Table' => array(
+            'By main categories' => 'pcs_table_inbreuk_per_plaats',
+            'By criminal figure' => 'pcs_table_crimfig_per_plaats',
+            'By police zone' => 'pcs_table_crimfig_per_pz'
+        ),
+        'Map' => array(
+            'Car theft' => 'pcs_mapchoropleth_crimfig_autodiefstal',
+            'Theft from vehicle' => 'pcs_mapchoropleth_crimfig_diefstaluitofaanvoertuig',
+            'Housebreaking' => 'pcs_mapchoropleth_crimfig_woninginbraakstrikt',
+            'Intrafamily voilence' => 'pcs_mapchoropleth_crimfig_ifgalgemeentotaal'
+        ),
+        'Graph' => array(
+            'Histogram main categories' => 'pcs_barchartvtime_inbreuk_tijd_inbreuken',
+            'Timeline criminal figure' => 'pcs_timeline_crimfig_tijd_crimfiguren'
+        )
+    );
+?>
+
+<? foreach($graphs as $key => $value) : ?>
+<div class="one-fourth">
+    <h3><?= translate($key) ?></h3>
+    <ul class="nav nav--pills">
+    <? foreach($value as $key => $value) : ?>
+        <li<?= isset($graph) && $graph == $value ? ' class="active"' : '' ?>>
+            <a href="?view=interactive&graph=<?= $value ?>"><?= translate($key) ?></a>
+        </li>
+    <? endforeach ?>
+    </ul>
+</div>
+<? endforeach ?>
