14e384e46e1c7742f5a97a84725dd6f641b1c2fb
Tom Janssens <tom@timble.net>
2015-04-14 15:45:15 +0200

    #126 - Removed news from intro page

--- application/site/component/streets/view/municipalities/templates/intro.html.php
+++ application/site/component/streets/view/municipalities/templates/intro.html.php
@@ -59,27 +59,7 @@
         </div>
     </div>
 
-    <div class="intro__news">
-        <div class="intro__container">
-            <? foreach ($newsitems as $item) : ?>
-                <? $article = $item->data; ?>
-                <div class="intro__news__item card">
-                    <a class="card__box" href="<?= $streams[$language]['news'].'/'.$article->id.'-'.$article->slug ?>">
-                        <div class="card__image">
-                        <? if($article->thumbnail): ?>
-                            <img width="400px" height="300px" src="<?= 'http://'.object('request')->getUrl()->getHost().'/files/fed/attachments/'.str_replace('.', '_thumb.', $article->thumbnail) ?>" />
-                        <? else : ?>
-                            <img src="assets://news/images/placeholder.png" />
-                        <? endif ?>
-                        </div>
-                        <div class="card__metadata">
-                            <span class="card__name"><?= $article->title ?></span>
-                        </div>
-                    </a>
-                </div>
-            <? endforeach ?>
-        </div>
-    </div>
+    <? //import('intro_news.html') ?>
 
     <meta content="noimageindex" name="robots" />
 
