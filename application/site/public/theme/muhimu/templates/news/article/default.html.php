84bb12b28dc69d976b0a999312dbe6e1468ebd6e
Tom Janssens <tom@timble.net>
2017-02-08 11:37:42 +0100

    #23: Move Javascript assets to appropriate view

--- application/site/public/theme/muhimu/templates/news/article/default.html.php
+++ application/site/public/theme/muhimu/templates/news/article/default.html.php
@@ -61,17 +61,3 @@
         <?= import('com:attachments.view.attachments.default.html', array('attachments' => $attachments, 'exclude' => array($article->attachments_attachment_id))) ?>
     </div>
 </article>
-
-<script src="assets://application/components/jquery/dist/jquery.min.js" />
-<script src="assets://application/components/magnific-popup/dist/jquery.magnific-popup.min.js" />
-<script data-inline>
-    $(document).ready(function() {
-        // This will create a single gallery from all elements that have class data-gallery="enabled"
-        $('[data-gallery="enabled"]').magnificPopup({
-            type: 'image',
-            gallery:{
-                enabled:true
-            }
-        });
-    });
-</script>
