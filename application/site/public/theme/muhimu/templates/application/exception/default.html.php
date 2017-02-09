46994ca854b85c2fe8c4c836e6df28041aca1d37
Tom Janssens <tom@timble.net>
2017-02-09 12:02:57 +0100

    #23: Style exception page

--- application/site/public/theme/muhimu/templates/application/exception/default.html.php
+++ application/site/public/theme/muhimu/templates/application/exception/default.html.php
@@ -18,7 +18,7 @@
 <html lang="<?= $language; ?>" dir="<?= $direction; ?>">
 <head>
     <base href="<?= url(); ?>" />
-    <title><?= $code; ?> - <?= translate('Page not found') ?></title>
+    <title><?= translate('Page not found') ?> - <?= $code; ?></title>
 
     <ktml:title>
     <ktml:style>
@@ -45,60 +45,68 @@
 </head>
 
 <body>
-<div id="wrap">
-    <div class="container container__header">
-        <div class="header">
-            <div class="organization" itemscope itemtype="http://schema.org/Organization">
-                <a itemprop="url" href="/<?= $site ?>">
-                    <div class="organization__logo organization__logo--<?= $language_short; ?>"></div>
-                    <div class="organization__name"><?= escape($zone->title); ?></div>
-                </a>
-            </div>
-            <div class="navigation">
-                <span class="slogan">
-                    <?= JText::sprintf('Call for urgent police assistance', '101', '101') ?>.
-                    <?= JText::sprintf('No emergency, just police', escape(str_replace(' ', '', $zone->phone_emergency)), escape($zone->phone_emergency)) ?>.
+<div id="wrapper">
+    <div class="skip">
+        <div class="container__skip">
+            <a href="#content">Skip to main content</a>
+        </div>
+    </div>
+    <div class="container__top">
+        <div class="organization" itemscope itemtype="http://schema.org/Organization">
+            <a itemprop="url" href="/<?= $site ?>">
+                <span class="organization__logo"></span>
+                <span class="organization__name">
+                    <span><?= escape($zone->title); ?></span>
                 </span>
-            </div>
+                <meta itemprop="logo" content="assets://application/images/logo-<?= array_shift(str_split($language, 2)); ?>.png" />
+            </a>
+        </div>
+        <div class="search">
+            <form>
+                <input type="search" placeholder="<?= translate('Search') ?>" />
+                <button type="submit" />
+            </form>
         </div>
     </div>
 
-    <div class="container container__content">
-        <h1><?= translate('Page not found') ?> - <?= $code ?></h1>
+    <div class="container__breadcrumb">
+        <ul class="breadcrumb">
+            <li><a href="/<?= $site ?>" class="pathway">Home</a></li>
+        </ul>
+    </div>
 
-        <div class="exception">
-            <div class="exception__message component">
-                <p><strong><?= translate('You may not be able to visit this page because of'); ?>:</strong></p>
-                <ul>
-                    <li><?= translate('An out-of-date bookmark/favourite'); ?></li>
-                    <li><?= translate('A search engine that has an out-of-date listing for this site'); ?></li>
-                    <li><?= translate('A mis-typed address'); ?></li>
-                    <li><?= translate('The requested resource was not found'); ?></li>
-                    <li><?= translate('An error has occurred while processing your request'); ?></li>
-                </ul>
-                <p><strong><?= translate('Please try one of the following pages'); ?>:</strong></p>
-                <ul>
-                    <li><a href="/<?= $site ?>" title="<?= translate('Home Page'); ?>"><?= translate('Home Page'); ?></a></li>
-                    <li><a href="/<?= $site ?>/contact" title="<?= translate('Contact'); ?>"><?= translate('Contact'); ?></a></li>
-                </ul>
-            </div>
-            <div class="exception__image">
-                <img class="thumbnail" src="assets://application/images/error.jpg" />
-            </div>
-        </div>
+    <div id="content" class="container container__content">
+        <div class="component component--no-sidebar">
+            <article class="article">
+                <h1 class="article__header"><?= translate('Page not found') ?></h1>
 
-        <div class="text-center" style="margin-top: 30px; clear: both">
-            <a class="button button--primary button--large" href="/<?= $site ?>" title="<?= translate('Home Page'); ?>"><?= translate('Home Page'); ?></a>
-        </div>
+                <div class="exception">
+                    <div class="exception__message component">
+                        <p><strong><?= translate('You may not be able to visit this page because of'); ?>:</strong></p>
+                        <ul>
+                            <li><?= translate('An out-of-date bookmark/favourite'); ?></li>
+                            <li><?= translate('A search engine that has an out-of-date listing for this site'); ?></li>
+                            <li><?= translate('A mis-typed address'); ?></li>
+                            <li><?= translate('The requested resource was not found'); ?></li>
+                            <li><?= translate('An error has occurred while processing your request'); ?></li>
+                        </ul>
+                    </div>
+                </div>
 
-        <? if(count($trace)) : ?>
-        <div class="backtrace">
-            <button id="backtrace__button" class="button--link" onclick="toggleBacktrace()" data-text-less="<?= translate('Less') ?>" data-text-more="<?= translate('More') ?>"><?= translate('More') ?></button>
+                <p style="margin-top: 30px; clear: both">
+                    <a class="button button--primary" href="/<?= $site ?>" title="<?= translate('Go to the homepage'); ?>"><?= translate('Go to the homepage'); ?></a>
+                </p>
+
+                <? if(count($trace)) : ?>
+                <div class="backtrace">
+                    <button id="backtrace__button" class="button--link" onclick="toggleBacktrace()" data-text-less="<?= translate('Less') ?>" data-text-more="<?= translate('More') ?>"><?= translate('More') ?></button>
+                </div>
+                <div id="backtrace__info" class="is-hidden">
+                    <?= import('default_backtrace.html'); ?>
+                </div>
+                <? endif; ?>
+            </article>
         </div>
-        <div id="backtrace__info" class="is-hidden">
-            <?= import('default_backtrace.html'); ?>
-        </div>
-        <? endif; ?>
     </div>
 </div>
 
