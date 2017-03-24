79b9c27b0d3653dc4960febee57e8c64f037aab2
Tom Janssens <tom@timble.net>
2017-03-24 16:37:27 +0100

    #23: Removed empty stylesheet

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,8 +25,7 @@ module.exports = function(grunt) {
                 files: {
                     'application/site/public/theme/muhimu/css/default.css': 'application/site/public/theme/muhimu/css/default.scss',
                     'application/site/public/theme/muhimu/css/fonts.css': 'application/site/public/theme/muhimu/css/fonts.scss',
-                    'application/site/public/theme/muhimu/css/ie.css': 'application/site/public/theme/muhimu/css/ie.scss',
-                    'application/site/public/theme/muhimu/css/ie7.css': 'application/site/public/theme/muhimu/css/ie7.scss'
+                    'application/site/public/theme/muhimu/css/ie.css': 'application/site/public/theme/muhimu/css/ie.scss'
                 }
             }
         },
