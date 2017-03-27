f1855b7b42cbe5eb8438dc1e6e972622a2d08742
Tom Janssens <tom@timble.net>
2017-03-27 20:31:47 +0200

    #235: Update link in docblocks

--- install/sample/generate.php
+++ install/sample/generate.php
@@ -5,7 +5,7 @@
  *
  * @copyright	Copyright (C) 2011 - 2017 Johan Janssens and Timble CVBA. (http://www.timble.net)
  * @license		GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
- * @link		git://git.assembla.com/nooku-framework.git for the canonical source repository
+ * @link		https://github.com/timble/openpolice-platform
  */
 
 use Nooku\Library;
